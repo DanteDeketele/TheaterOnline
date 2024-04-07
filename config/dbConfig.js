14835e50b34c8f69f62446ab6422ea943e2f8c9a
Dante Deketele <86111685+DanteDeketele@users.noreply.github.com>
2024-04-07 19:46:49 +0200

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,10 +2,11 @@
 
 const mysql = require('mysql2');
 
+// TODO: HIDE THIS INFO IN ENV
 const dbConnection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'v4H5D4U8IYec0IcYJMeA',
+  password: 'v4H5D4U8IYec0IcYJMeA', //Yes this is not safe, but its a test database at the moment
   database: 'theateronlineapi', // Adjust the database name as needed
 });
 
