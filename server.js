55f90e173ed71d924237d839eebe1d6f99626eb1
Dante Deketele <mrdeegames@gmail.com>
2024-04-10 14:06:50 +0200

    Bootstrap

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const session = require('express-session');
+const expressLayouts = require('express-ejs-layouts')
 const path = require('path');
 const cors = require('cors');
 const userRoutes = require('./routes/userRoutes');
@@ -28,14 +29,18 @@ app.use(session({
 app.use(express.json());
 
 
-// Set the view engine to EJS
-app.set('view engine', 'ejs');
-
-// Set the views directory
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts)
+app.set('layout', './layouts/main')
+app.set('view engine', 'ejs')
+
+app.use(expressLayouts);
 
 // Set up static file serving
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve Bootstrap files from node_modules
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist/'));
 
 
 
@@ -43,9 +48,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/user', userRoutes);
 app.use('/api/producer', producerRoutes);
 
+// Apply the middleware to all site routes
+app.use('/', fetchUserDetails);
 
-// Apply the middleware to all admin routes
-app.use('/admin', fetchUserDetails);
+app.get('/', (req, res) => {
+  var locals = {
+    title: 'Home',
+    description: 'Welcome to the home page',
+    header: 'Home Page',
+    user: req.user
+  };
+  res.render('pages/home', locals);
+});
+
+app.get('/map', (req, res) => {
+  res.render('pages/map', { user: req.user });
+});
+
 
 // admin routes ---------------------------->
 app.get('/admin', (req, res) => {
@@ -64,9 +83,7 @@ app.get('/admin/users/:id', (req, res) => {
   res.render('admin/user', { user: req.user, id: req.params.id });
 });
 
-app.get('/map', (req, res) => {
-  res.render('map', { user: req.user });
-});
+
 
 // Middleware to handle 404 errors
 app.use((req, res, next) => {
